aab0ffe567f99e95d2adb98b532a1f61def292ad
Jonas Willian <jwnarancity@gmail.com>
2023-11-17 19:41:23 -0300

    Instalação do ngxToastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { CursosModule } from './cursos/cursos.module';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { CursosModule } from './cursos/cursos.module';
 import { HomeComponent } from './pages/home/home.component';
 import { ContatoComponent } from './pages/contato/contato.component';
 
@@ -25,7 +26,8 @@ import { ContatoComponent } from './pages/contato/contato.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    CursosModule
+    CursosModule,
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
