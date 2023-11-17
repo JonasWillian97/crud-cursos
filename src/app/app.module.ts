a1eecc6ef79309f9e133fe7e68a567b04697a3f2
Jonas Willian <jwnarancity@gmail.com>
2023-11-17 11:20:12 -0300

    Criação e estilização do footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    FooterComponent,
   
   ],
   imports: [
